7bf86de4fee0e48bcf7dad66582f0d4d08a01ec5
GabrielBursi <gabriellbursii@gmail.com>
2022-07-19 22:10:19 -0300

    criando as funçoes no js

--- script1.js
+++ script1.js
@@ -1,13 +1,51 @@
 class Produtos{
     constructor(){
-        
+        this.id=1
+        this.arrayProdutos=[]
     }
+
     salvar(){
-        
+       let produto = this.lerDados()
+       if(this.isEmpty(produto)){
+        this.adicionaArray(produto)
+        console.log(this.arrayProdutos)
+       }
     }
+
+    adicionaArray(produto){
+        this.arrayProdutos.push(produto)
+        this.id++
+    }
+
+    lerDados(){
+        let produto = {}
+
+        produto.id=this.id
+        produto.nomeProduto=document.getElementById("nome-produto").value
+        produto.valorProduto=document.getElementById("valor-produto").value
+
+        return produto;
+    }
+
+    isEmpty(produto){
+        let msg = ""
+        if(produto.nomeProduto == ""){
+            msg+= "Informe o nome do produto!\n"
+        }
+        if(produto.valorProduto == ""){
+            msg+= "Informe o preço do produto!\n"
+        }
+        if(msg != ""){
+            alert(msg)
+            return false
+        }
+        return true;
+    }
+
     cancelar(){
         
     }
+    
     editar(){
 
     }
