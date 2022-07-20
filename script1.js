754e699c352305ab90dde1b803b882a5394a3021
GabrielBursi <gabriellbursii@gmail.com>
2022-07-19 21:52:40 -0300

    corrigindo o codido js

--- script1.js
+++ script1.js
@@ -1,17 +1,15 @@
-class Produto{
+class Produtos{
     constructor(){
-        this.id = id;
-        this.nome = nome;
-        this.valor = valor
+        
     }
     salvar(){
-        alert("teste")
+        
     }
     cancelar(){
-
+        
     }
     editar(){
 
     }
 }
-var produto = new Produto();
+var produto = new Produtos();
