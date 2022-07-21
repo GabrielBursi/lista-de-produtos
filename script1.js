fe1a7134e6d56a3af0b74fa363d97f3f25d911f8
GabrielBursi <gabriellbursii@gmail.com>
2022-07-21 19:47:37 -0300

    funçao de editar o produto

--- script1.js
+++ script1.js
@@ -2,12 +2,18 @@ class Produtos{
     constructor(){
         this.id=1
         this.arrayProdutos=[]
+        this.editId=null
     }
 
     salvar(){
        let produto = this.lerDados()
        if(this.isEmpty(produto)){
-        this.adicionaArray(produto)
+        if(this.editId==null){
+            this.adicionaArray(produto)
+        }else{
+            this.atualizar(this.editId, produto)
+        }
+        
        }
        this.listaTabela()
        this.cancelar()
@@ -31,6 +37,7 @@ class Produtos{
             
             let iconEdit = document.createElement('img')
             iconEdit.src = "https://cdn-icons.flaticon.com/png/512/4103/premium/4103111.png?token=exp=1658266431~hmac=c65e680c4bf9158bc9236efe33f1d888"
+            iconEdit.setAttribute("onclick","produto.editar("+ JSON.stringify(this.arrayProdutos[i]) +")")
 
             let iconDel = document.createElement("img")
             iconDel.src = "https://cdn-icons.flaticon.com/png/512/3138/premium/3138336.png?token=exp=1658266390~hmac=eef2d7e07c0cc951f99482e2d1c25756"
@@ -44,6 +51,15 @@ class Produtos{
     adicionaArray(produto){
         this.arrayProdutos.push(produto)
         this.id++
+    }
+
+    atualizar(id, produto){
+        for(var i=0; i < this.arrayProdutos.length; i++){
+            if(this.arrayProdutos[i].id == id){
+                this.arrayProdutos[i].nomeProduto = produto.nomeProduto
+                this.arrayProdutos[i].valorProduto = produto.valorProduto
+            }
+        }
     }
 
     lerDados(){
@@ -74,22 +90,32 @@ class Produtos{
     cancelar(){
         document.getElementById("nome-produto").value = ""
         document.getElementById("valor-produto").value = ""
+
+        document.getElementById("atualizar").innerText = "Salvar"
+        this.editId = null
     }
     
-    editar(){
+    editar(dados){
+        this.editId = dados.id
 
+        document.getElementById("nome-produto").value=dados.nomeProduto
+        document.getElementById("valor-produto").value=dados.valorProduto
+
+        document.getElementById("atualizar").innerText = "Atualizar"
     }
 
     deletar(id){
-        //alert(`${id}`)
-        let tbody = document.getElementById("tbody")
+        if(confirm(`Tem certeza que quer realmente excluir esse produto: ${id} ?`)){
+            let tbody = document.getElementById("tbody")
 
-        for(var i=0; i < this.arrayProdutos.length; i++){
+            for(var i=0; i < this.arrayProdutos.length; i++){
             if(this.arrayProdutos[i].id == id){
                 this.arrayProdutos.splice(i, 1)
                 tbody.deleteRow(i)
             }
         }
+        }
+        
     }
 }
 var produto = new Produtos();
